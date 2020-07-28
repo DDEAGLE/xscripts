ef050e22214e3b804fc1f069968ad86543e7872e
xoroxx <hairoeszaman@gmail.com>
2020-07-28 13:43:29 +0700

    add files

---
+++ circleci.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+KERNELNAME=DesertEagle
+KERNELVERSION=test
+DEVICES=whyred,tulip,wayne
+TOOLCHAIN=clang
+
+export KBUILD_BUILD_USER=builder
+export KBUILD_BUILD_HOST=deagle
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/tools.sh
+
+if [ $TOOLCHAIN == "clang" ]; then
+	git clone --depth=1 https://github.com/NusantaraDevs/clang.git -b dev/11.0 clang
+	START=$(date +"%s")
+	sendmsg_intro $KERNELVERSION $TOOLCHAIN
+
+	for i in ${DEVICES//,/ }
+	do
+		build_clang "deserteagle_${i}_defconfig"
+		BUILDDATE=$(date +"%y%m%d-%H%M")
+        	ZIPNAME="${KERNELNAME}-${i}-${KERNELVERSION}-${BUILDDATE}.zip"
+        	zipper ${i} $ZIPNAME
+		sendmsg_file $ZIPNAME
+		build_clean
+	done
+
+	END=$(date +"%s")
+	sendmsg_finish $START $END
+else
+	exit 1
+fi
---
+++ tools.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+build_clang(){
+	echo  "Building $1 kernel with clang"
+
+	make O=out ARCH=arm64 $1
+	PATH="$(pwd)/clang/bin:${PATH}" \
+	make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+build_clean(){
+	rm -rf $(pwd)/out
+}
+
+sendmsg_intro(){
+	curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage \
+		-d parse_mode=HTML \
+		-d chat_id=${CHANNEL} \
+		-d text="<b>🔥 Building Karamel Now ...!</b>
+		<b>+ Version:</b> <code>$1</code>
+		<b>+ Compiler:</b> <code>$2</code>
+		<b>+ Branch:</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>
+		<b>+ Commit:</b> <code>$(git log --pretty=format:'%h : %s' -1)</code>"
+}
+
+sendmsg_finish(){
+	DIFF=$(( $2 - $1 ))
+	curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage \
+                -d chat_id=${CHANNEL} \
+                -d parse_mode=HTML \
+                -d text="💥 Build completed in $((DIFF / 60)) minutes $((DIFF % 60)) seconds"
+}
+
+sendmsg_error(){
+	curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage \
+		-d text="⛔ Build failed with errors..." \
+		-d chat_id=${CHANNEL} \
+		-d parse_mode=HTML
+	exit 1
+}
+
+sendmsg_file(){
+	file=$(pwd)/$1
+	curl -F chat_id="${CHANNEL}" \
+             	-F caption="sha1sum : $(sha1sum ${file} | awk '{ print $1 }')" \
+                -F document=@"${file}" \
+                 https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
+}
+
+zipper(){
+	git clone --depth=1 https://github.com/xoroxx/AnyKernel3 -b $1 anykernel
+
+	if [ -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]; then
+		cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel
+		cd anykernel
+		zip -r ../$2 *
+		cd ..
+		rm -rf anykernel
+	else
+		sendmsg_error
+	fi
+}
